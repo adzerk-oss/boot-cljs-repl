9622a14019305e5291a7e86c5a009b949ecba455
Micha Niskin <micha.niskin@gmail.com>
2014-10-23 13:10:20 -0400

    Add -h option to set ws host; delay deps resolution

--- src/tailrecursion/boot_cljs_repl.clj
+++ src/tailrecursion/boot_cljs_repl.clj
@@ -16,32 +16,35 @@
   [ns coord]
   `(when-not (guard (do (require '~ns) :ok)) '~coord))
 
+(def ws-host  (atom nil))
 (def ws-port  (atom 0))
 (def out-file (atom nil))
 (def continue (atom nil))
-(def event    (atom nil))
 
 (def deps 
-  (concat
-    (whendep cemerick.piggieback   [[com.cemerick/piggieback   "0.1.3"]])
-    (whendep weasel.repl.websocket [[weasel                    "0.4.1"]])
-    (whendep cljs.analyzer         [[org.clojure/clojurescript "0.0-2371"]])))
+  (delay
+    (concat
+      (whendep cemerick.piggieback   [[com.cemerick/piggieback   "0.1.3"]])
+      (whendep weasel.repl.websocket [[weasel                    "0.4.1"]])
+      (whendep cljs.analyzer         [[org.clojure/clojurescript "0.0-2371"]]))))
 
 (defn start-repl
   []
-  (with-out-str
-    ((r cemerick.piggieback/cljs-repl)
-     :repl-env ((r weasel.repl.websocket/repl-env) :port @ws-port)))
-  (let [conn (->> @@(r weasel.repl.server/state)
-               :server meta :local-port (format "ws://localhost:%d"))]
-    (info "<< started Weasel server on %s >>\n\n" conn)
+  (let [mesg (with-out-str
+               (->> (when @ws-host [:ip @ws-host])
+                 (apply (r weasel.repl.websocket/repl-env) :port @ws-port)
+                 ((r cemerick.piggieback/cljs-repl) :repl-env)))
+        port (->> @@(r weasel.repl.server/state) :server meta :local-port)
+        conn (format "ws://%s:%d" (or @ws-host "localhost") port)]
+    (info (.replaceAll mesg ":[0-9]+ >>" (format ":%d >>" port)))
     (io/make-parents @out-file)
     (->> (template
            ((ns tailrecursion.boot-cljs-repl
               (:require [weasel.repl :as repl]))
             (when-not (repl/alive?) (repl/connect ~conn))))
       (map pr-str) (interpose "\n") (apply str) (spit @out-file))
-    (@continue (make-event @event))))
+    (touch @out-file)
+    (@continue (make-event))))
 
 (deftask cljs-repl
   "Start a ClojureScript REPL server.
@@ -49,11 +52,13 @@
   The default configuration starts a websocket server on a random available
   port on localhost."
 
-  [p port PORT int "The port the websocket server listens on."]
+  [H host ADDR str "The IP address for the server to listen on."
+   p port PORT int "The port the websocket server listens on."]
 
   (let [src (mksrcdir!)]
     (when port (reset! ws-port port))
-    (when (seq deps) (set-env! :dependencies #(into % deps)))
+    (when host (reset! ws-host host))
+    (when (seq @deps) (set-env! :dependencies #(into % @deps)))
     (reset! out-file (io/file src "tailrecursion" "boot_cljs_repl.cljs"))
     (comp
       (repl
@@ -61,6 +66,5 @@
         :middleware [(r cemerick.piggieback/wrap-cljs-repl)])
       (fn [continue*]
         (fn [event*]
-          (reset! event event*)
           (reset! continue continue*)
           (continue* event*))))))
