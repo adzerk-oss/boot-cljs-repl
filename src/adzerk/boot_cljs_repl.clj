7ee48c75d81d31b6463330e3ab473d8523efcbab
Micha Niskin <micha.niskin@gmail.com>
2015-01-07 21:18:31 -0500

    Emit client code to :source-paths, add support for .main.edn files

--- src/adzerk/boot_cljs_repl.clj
+++ src/adzerk/boot_cljs_repl.clj
@@ -15,7 +15,6 @@
 (def ^:private ws-ip    (atom nil))
 (def ^:private ws-port  (atom 0))
 (def ^:private out-file (atom nil))
-(def ^:private continue (atom nil))
 
 (def ^:private deps
   (delay (remove pod/dependency-loaded? '[[com.cemerick/piggieback   "0.1.3"]
@@ -25,21 +24,28 @@
 (defn- make-repl-connect-file
   [conn]
   (io/make-parents @out-file)
+  (info "Writing %s...\n" (.getName @out-file))
   (->> (template
          ((ns adzerk.boot-cljs-repl
             (:require [weasel.repl :as repl]))
-          (when-not (repl/alive?) (repl/connect ~conn))))
-       (map pr-str) (interpose "\n") (apply str) (spit @out-file))
-  (touch @out-file))
+          (let [repl-conn ~conn]
+            (when (and repl-conn (not (repl/alive?)))
+              (repl/connect ~conn)))))
+       (map pr-str) (interpose "\n") (apply str) (spit @out-file)))
 
 (defn- weasel-connection
   [ip port]
-  (->> (when ip [:ip ip])
-       (apply (r weasel.repl.websocket/repl-env) :port port)))
+  (apply (r weasel.repl.websocket/repl-env) :port port (when ip [:ip ip])))
 
 (defn- weasel-port
   []
   (->> @@(r weasel.repl.server/state) :server meta :local-port))
+
+(defn- weasel-stop
+  []
+  (when-let [stop (:server @@(r weasel.repl.server/state))]
+    (info "<< stopping repl websocket server >>\n")
+    (stop)))
 
 (defn repl-env
   "Start the Weasel server without attaching a REPL client immediately. This will
@@ -56,7 +62,6 @@
         port     (weasel-port)
         conn     (format "ws://%s:%d" clih port)]
     (make-repl-connect-file conn)
-    (@continue)
     repl-env))
 
 (defn start-repl
@@ -68,20 +73,23 @@
   [& {i :ip p :port}]
   (let [i    (or i @ws-ip)
         p    (or p @ws-port)
-        clih (if (and i (not= i "0.0.0.0")) i "localhost")
-        mesg (with-out-str
-               (->> (weasel-connection i p)
-                    ((r cemerick.piggieback/cljs-repl) :repl-env)))
-        port (weasel-port)
-        conn (format "ws://%s:%d" clih port)]
-    (info (.replaceAll mesg ":[0-9]+ >>" (format ":%d >>" port)))
-    (make-repl-connect-file conn)
-    (@continue)
-    nil))
+        clih (if (and i (not= i "0.0.0.0")) i "localhost")]
+    ((r cemerick.piggieback/cljs-repl) :repl-env (weasel-connection i p))
+    (let [port (weasel-port)
+          conn (format "ws://%s:%d" clih port)]
+      (make-repl-connect-file conn)
+      nil)))
 
-(defn- make-continue
-  [next-handler fileset src]
-  #(do (reset-build!) (-> fileset (add-source src) commit! next-handler)))
+(defn- add-init!
+  [in-file out-file]
+  (let [ns 'adzerk.boot-cljs-repl]
+    (info "Adding :require %s to %s...\n" ns (.getName in-file))
+    (-> in-file
+        slurp
+        read-string
+        (update-in [:require] conj ns)
+        pr-str
+        ((partial spit out-file)))))
 
 (deftask cljs-repl
   "Start a ClojureScript REPL server.
@@ -92,16 +100,22 @@
   [i ip ADDR   str "The IP address for the server to listen on."
    p port PORT int "The port the websocket server listens on."]
 
-  (let [src (temp-dir!)]
+  (let [src (temp-dir!)
+        tmp (temp-dir!)]
+    (cleanup (weasel-stop))
     (when ip (reset! ws-ip ip))
     (when port (reset! ws-port port))
-    (when (seq @deps) (set-env! :dependencies #(into % @deps)))
+    (set-env! :source-paths #(conj % (.getPath src))
+              :dependencies #(into % (vec (seq @deps))))
     (reset! out-file (io/file src "adzerk" "boot_cljs_repl.cljs"))
+    (make-repl-connect-file nil)
     (comp
-      (repl
-        :server     true
-        :middleware ['cemerick.piggieback/wrap-cljs-repl])
-      (fn [next-handler]
-        (fn [fileset]
-          (reset! continue (make-continue next-handler fileset src))
-          (-> fileset (add-source src) commit! next-handler))))))
+      (repl :server     true
+            :middleware ['cemerick.piggieback/wrap-cljs-repl])
+      (with-pre-wrap fileset
+        (doseq [f (->> fileset input-files (by-ext [".main.edn"]))]
+          (let [path (tmppath f)
+                in-file (tmpfile f)
+                out-file (io/file tmp path)]
+            (add-init! in-file out-file)))
+        (-> fileset (add-resource tmp) commit!)))))
